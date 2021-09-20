deef688f9a21d4d274a358bf4783e9c894c563a9
Aaron Hennessey <axelancerr@gmail.com>
2021-09-20 11:59:34 +0100

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,18 @@
+
+def maths(a: float, b: float, *, operation: str) -> float:
+    if operation == "*":
+        return a * b
+    elif operation == "/":
+        return a / b
+    elif operation == "-":
+        return a - b
+    elif operation == "+":
+        return a + b
+    raise ValueError("Invalid operation")
+
+print(maths(10, 5, operation="*"))
+print(maths(20, 5, operation="/"))
+print(maths(1, 5, operation="-"))
+print(maths(20, 7, operation="-"))
+
+    
