26cdc303a304408715e0ee1d97df925328c80959
Aaron Hennessey <axelancerr@gmail.com>
2021-09-21 15:39:18 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 
-def maths(a: float, b: float, *, operation: str) -> float:
+def calculator(a: float, b: float, *, operation: str) -> float:
     if operation == "*":
         return a * b
     elif operation == "/":
@@ -10,7 +10,7 @@ def maths(a: float, b: float, *, operation: str) -> float:
         return a + b
     raise ValueError("Invalid operation")
 
-print(maths(10, 5, operation="*"))
-print(maths(20, 5, operation="/"))
-print(maths(1, 5, operation="-"))
-print(maths(20, 7, operation="+"))
+print(calculator(10, 5, operation="*"))
+print(calculator(20, 5, operation="/"))
+print(calculator(1, 5, operation="-"))
+print(calculator(20, 7, operation="+"))
