85706c1c6f909c45696c68239b3672d7a4acd497
Aaron Hennessey <axelancerr@gmail.com>
2022-02-08 11:27:18 +0000

    enums and stuff bro

--- Programming/Lesson11/Program.cs
+++ Programming/Lesson11/Program.cs
@@ -4,9 +4,65 @@ namespace Lesson11
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void PrintMixMaxDatatypeValues()
         {
+            Console.WriteLine($"Min short: {short.MinValue}");
+            Console.WriteLine($"Max short: {short.MaxValue}");
+            Console.WriteLine($"Min ushort: {ushort.MinValue}");
+            Console.WriteLine($"Max ushort: {ushort.MaxValue}");
             
+            Console.WriteLine($"Min int: {int.MinValue}");
+            Console.WriteLine($"Max int: {int.MaxValue}");
+            Console.WriteLine($"Min uint: {uint.MinValue}");
+            Console.WriteLine($"Max int: {uint.MaxValue}");
+            
+            Console.WriteLine($"Min long: {long.MinValue}");
+            Console.WriteLine($"Max long: {long.MaxValue}");
+            Console.WriteLine($"Min ushort: {ulong.MinValue}");
+            Console.WriteLine($"Max ushort: {ulong.MaxValue}");
+        }
+
+        private enum Weekdays
+        {
+            Monday = 0,
+            Tuesday = 1,
+            Wednesday = 2,
+            Thursday = 3,
+            Friday = 4,
+            Saturday = 5,
+            Sunday = 6,
+        }
+
+        private enum GameState
+        {
+            PLAY = 0,
+            HELP = 1,
+            EXIT = 2,
+        }
+        
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            Array values = typeof(GameState).GetEnumValues();
+            
+            GameState state = (GameState)values.GetValue(random.Next(values.Length));
+
+            switch (state)
+            {
+                case GameState.PLAY:
+                    Console.WriteLine("playing");
+                    break;
+                case GameState.HELP:
+                    Console.WriteLine("help menu");
+                    break;
+                case GameState.EXIT:
+                    Console.WriteLine("exiting");
+                    break;
+                default:
+                    Console.WriteLine("unknown state");
+                    break;
+            }
+
         }
     }
 }
